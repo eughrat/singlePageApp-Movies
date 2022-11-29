8b8c6addea178a46782bce92297c723beac39ced
eughrat <michal.piernicki@gmail.com>
2022-11-29 15:47:30 +0100

    first commit

---
+++ src/app/detale/detale.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DetaleComponent } from './detale.component';
+
+describe('DetaleComponent', () => {
+  let component: DetaleComponent;
+  let fixture: ComponentFixture<DetaleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DetaleComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DetaleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/detale/detale.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detale',
+  templateUrl: './detale.component.html',
+  styleUrls: ['./detale.component.css']
+})
+export class DetaleComponent {
+
+}
